26d60022fb485682ad3f770184d8e5d6da9d4996
Daniel Weller <daniel.weller@softwarepark.cc>
2019-10-16 08:22:47 +0200

    Added optional DB init scripts.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@ docker_run="docker run"
 docker_run="$docker_run -e POSTGRES_DB=$INPUT_POSTGRESQL_DB"
 docker_run="$docker_run -e POSTGRES_USER=$INPUT_POSTGRESQL_USER"
 docker_run="$docker_run -e POSTGRES_PASSWORD=$INPUT_POSTGRESQL_PASSWORD"
+
+if [ ! -z "$INPUT_POSTGRESQL_VOLUME" ]
+then
+  PWD=`pwd`
+  docker_run="$docker_run -v $PWD/$INPUT_POSTGRESQL_INIT_SCRIPTS:/docker-entrypoint-initdb.d"
+fi
+
 docker_run="$docker_run -d -p 5432:5432 postgres:$INPUT_POSTGRESQL_VERSION"
 
 sh -c "$docker_run"
